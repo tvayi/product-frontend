882a7d6b8f9d2642ac01c5ed625d9736cd32674a
Belyaev <semen.belyaev@improvado.io>
2023-11-07 07:04:19 +0300

    add new forms

--- src/App.js
+++ src/App.js
@@ -1,75 +1,167 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Card, ListGroup, Alert} from 'react-bootstrap';
 
 class MyComponent extends Component {
     constructor() {
         super();
         this.state = {
+            products: [],
             name: '',
-            duration: 0,
-            resource: 0,
-            id: '',
-            message: '',
+            weight: 0,
+            description: '',
             error: null,
+            formVisible: false,
+            order: [],
         };
     }
 
+    componentDidMount() {
+        Axios.get('/product')
+            .then(response => {
+                this.setState({ products: response.data });
+            })
+            .catch(error => {
+                console.log(error);
+
+                const mockResponse = [
+                    { id:1, name: 'product 1', weight: '20', description: 'Description 1' },
+                    { id:2, name: 'product 2', weight: '30', description: 'Description 2' },
+                    { id:3, name: 'product 3', weight: '40', description: 'Description 3' },
+                ];
+
+                this.setState({ products: mockResponse });
+            });
+    }
+
+    addToOrder = (product) => {
+        this.setState(prevState => ({
+            order: [...prevState.order, product]
+        }))
+    }
+
+    processOrder = () => {
+        Axios.post('/order', this.state.order)
+            .then(response => {
+                this.setState({ message: 'Order processed successfully.', error: null });
+            })
+            .catch(error => {
+                this.setState({
+                    error: `Order was added (mock response because server in unavailable) Products in order: ${JSON.stringify(this.state.order)}`,
+                    message: ''
+                });
+            });
+    }
     postData = (e) => {
         e.preventDefault();
-        const { name, duration, resource, id } = this.state;
-        Axios.post('/project', { name, duration, resource, id })
+        const { name, weight, description, id } = this.state;
+        Axios.post('/product', { name, weight, description, id })
             .then(response => {
                 this.setState({ message: 'Data submitted successfully.', error: null });
             })
             .catch(error => {
-                this.setState({ error: 'There was an error during submission.', message: '' });
+                this.setState({
+                    error: `Product was added (mock response because server in unavailable)`,
+                    message: ''
+                });
             });
     }
 
     handleInputChange = e => {
         let value = e.target.value;
 
-        if (e.target.name === 'duration' || e.target.name === 'resource') {
+        if (e.target.name === 'weight') {
             value = parseInt(value, 10);
         }
 
         this.setState({ [e.target.name]: value });
     }
 
+    toggleFormVisibility = () => {
+        this.setState(prevState => ({
+            formVisible: !prevState.formVisible
+        }));
+    }
+
+    renderForm = () => (
+        <Form onSubmit={this.postData}>
+            <Form.Group controlId="formName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" name="name" onChange={this.handleInputChange} placeholder="Enter product name" />
+            </Form.Group>
+
+            <Form.Group controlId="formWeight">
+                <Form.Label>Weight</Form.Label>
+                <Form.Control type="number" name="weight" onChange={this.handleInputChange} placeholder="Enter product weight" />
+            </Form.Group>
+
+            <Form.Group controlId="formDescription">
+                <Form.Label>Description</Form.Label>
+                <Form.Control type="text" name="description" onChange={this.handleInputChange} placeholder="Enter product description" />
+            </Form.Group>
+
+            <div>
+                { this.state.message && <p>{this.state.message}</p> }
+                { this.state.error && <p>{this.state.error}</p> }
+                <Button variant="primary" type="submit">Submit</Button>
+            </div>
+        </Form>
+    )
+
+    renderProductList = () => (
+        <ListGroup>
+            {this.state.products.map(product => (
+                <ListGroup.Item key={product.id}>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Title>{product.name}</Card.Title>
+                            <Card.Text>
+                                Weight: {product.weight} <br/>
+                                Description: {product.description}
+                            </Card.Text>
+                            <Button variant="primary" onClick={() => this.addToOrder(product)}>Add to Order</Button>
+                        </Card.Body>
+                    </Card>
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
+    )
+
+    renderAlert = () => {
+        const { message, error } = this.state;
+
+        if (message) {
+            return <Alert variant='success'>{message}</Alert>;
+        }
+
+        if (error) {
+            return <Alert variant='danger'>{error}</Alert>;
+        }
+
+        return null;
+    }
+
     render() {
         return (
-            <Form onSubmit={this.postData}>
-                <Form.Group controlId="formName">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" name="name" onChange={this.handleInputChange} placeholder="Enter Name" />
-                </Form.Group>
+            <div>
+                <Button variant="primary" onClick={this.toggleFormVisibility}>
+                    Add product
+                </Button>
 
-                <Form.Group controlId="formDuration">
-                    <Form.Label>Duration</Form.Label>
-                    <Form.Control type="number" name="duration" onChange={this.handleInputChange} placeholder="Enter Duration" />
-                </Form.Group>
-
-                <Form.Group controlId="formResource">
-                    <Form.Label>Resource</Form.Label>
-                    <Form.Control type="number" name="resource" onChange={this.handleInputChange} placeholder="Enter Resource" />
-                </Form.Group>
-
-                <Form.Group controlId="formId">
-                    <Form.Label>ID</Form.Label>
-                    <Form.Control type="text" name="id" onChange={this.handleInputChange} placeholder="Enter ID" />
-                </Form.Group>
-
+                {this.state.formVisible && this.renderForm()}
                 <div>
-                    { this.state.message && <p>{this.state.message}</p> }
-                    { this.state.error && <p>{this.state.error}</p> }
-                    <Button variant="primary" type="submit">Submit</Button>
+                    <h2>All Products</h2>
+                    {this.renderProductList()}
                 </div>
-            </Form>
-
+                <Button variant="secondary" onClick={this.processOrder} disabled={this.state.order.length === 0}>
+                    Process Order
+                </Button>
+                {this.renderAlert()}
+            </div>
         );
     }
 }
+
 
 export default MyComponent;
